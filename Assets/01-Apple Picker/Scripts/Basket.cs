3b825530713d218cadd1a402bb57fa32a5c1d498
Hamza <79418669+HA-Hamza-GITHUB@users.noreply.github.com>
2024-02-04 05:01:07 -0500

    Collection complete
    Up tll "Catching Apples" section - adjust freq. of apples.

--- Assets/01-Apple Picker/Scripts/Basket.cs
+++ Assets/01-Apple Picker/Scripts/Basket.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class Basket : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    // Update is called once per frame
+    void Update () {
+        // Get the current screen position of t
+        Vector3 mousePos2D = Input.mousePosition;
+        // The Camera's z position sets how far
+         mousePos2D.z = -Camera.main.transform.position.z;
+
+// Convert the point from 2D screen spa
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);    
+        Vector3 pos = this.transform.position;
+        pos.x = mousePos3D.x;
+        this.transform.position = pos;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void OnCollisionEnter(Collision coll) {  
+        // Find out what hit this basket
+        GameObject collidedWith = coll.gameObject;
+        if (collidedWith.tag == "Apple" ) {   
+            Destroy(collidedWith);
+        }
     }
+
 }
