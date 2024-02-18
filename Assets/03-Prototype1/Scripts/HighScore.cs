dc0249c7317661a97d46f94dfb2f491b47d295a6
Hamza <79418669+HA-Hamza-GITHUB@users.noreply.github.com>
2024-02-18 03:43:49 -0500

    Items Set
    Items Placed

---
+++ Assets/03-Prototype1/Scripts/HighScore.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class HighScore : MonoBehaviour
+{
+    static public int    score = 1000;
+    
+    void Start(){
+
+    }
+    void Awake() {                             
+        // If the PlayerPrefs HighScore already
+        if (PlayerPrefs.HasKey("HighScore")) {
+            score = PlayerPrefs.GetInt("HighScore");
+        }
+        // Assign the high score to HighScore
+        PlayerPrefs.SetInt("HighScore", score);
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        TMP_Text gt = this.GetComponent<TMP_Text>();
+        gt.text = "HighScore: " + score;
+        // Update the PlayerPrefs HighScore if 
+        if (score > PlayerPrefs.GetInt("HighScore"))
+        {
+            PlayerPrefs.SetInt("HighScore", score);
+        }
+    }
+}
